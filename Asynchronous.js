f390f29a5e3195f093f1c3c7dae09ccaaeb42520
Geno Joshua <133131809+GenoJ83@users.noreply.github.com>
2025-02-06 10:51:32 +0300

    Final

--- Asynchronous.js
+++ Asynchronous.js
@@ -3,4 +3,6 @@ console.log('Put the water to boil');
 console.log('Water is boiling');
 sertTimeout(()=> {
 
-})
+    console.log('Boiling water is poured into the cup');
+}, 5000)
+console.log('The tea is ready');
