a0cf1cbdae28c680457c66b778b5734749c4dc54
Geno Joshua <133131809+GenoJ83@users.noreply.github.com>
2025-02-10 15:31:56 +0300

    Initial

---
+++ JSON assignment/Async and wait.js
@@ -0,0 +1,15 @@
+const fs = require('fs').promises;
+const path = 'sample1.json';
+
+async function fetchDataAsync(filePath) {
+  try {
+    const data = await fs.readFile(filePath, 'utf8');
+    const jsonData = JSON.parse(data);
+    console.log('Async/Await Data from JSON file:', jsonData);
+  } catch (error) {
+    console.error(`Error reading or parsing file: ${error}`);
+  }
+}
+
+// Usage
+fetchDataAsync(path.join(__dirname, 'data.json'));
