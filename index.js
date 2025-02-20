4b251a43b61a4177f8388f0e943485ea802e1a42
Geno Joshua <133131809+GenoJ83@users.noreply.github.com>
2025-02-20 11:21:37 +0300

    function

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+function requestLogger(requestz, response, next){
+    console,log(``)
+}
 app.get('/', (req,res)=>{
     res.send('Hello World!');
 })
