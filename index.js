d2ff149617f8628145c752ddcdee5841d2c80f8b
Geno Joshua <133131809+GenoJ83@users.noreply.github.com>
2025-02-20 10:48:35 +0300

    hello

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+app.get('/', (req,res)=>{
+    res.send('Hello World!');
+})
 app.listen(port,()=>{
     console.log(`the server is running on ${port}`)
 
