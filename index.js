cf65ae7e4192fc61f2c9cf558c2325b85b6f66e4
Geno Joshua <133131809+GenoJ83@users.noreply.github.com>
2025-02-20 11:35:35 +0300

    Update

--- index.js
+++ index.js
@@ -3,15 +3,17 @@ const express = require('express');
 const app = express();
 const port = 8080;
 // Logging Middleware function
-function requestLogger(requestz, response, next){
-    console,log(`Request Method: ${request.method}), URL:${request.url}`);
-    next();// Passing control to the next middleware/ route handler
+function requestLogger(request, response, next){
+    console.log(`Request method: ${request.method}, URL: ${request.url}`);
+    next(); // Passing control to the next middleware/route handler
 }
-app.use(requestLogger); //we then apply the middleware to all routes
-app.get('/', (req,res)=>{
-    res.send('Hello World!');
-})
-app.listen(port,()=>{
-    console.log(`the server is running on ${port}`)
+app.use(requestLogger); // Apply the middleware to all routes
 
-})
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`The server is running on ${port}`);
+});
+
