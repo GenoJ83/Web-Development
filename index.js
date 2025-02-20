3c0abcfb5ffd8e699e271800c82a576bf527335f
Geno Joshua <133131809+GenoJ83@users.noreply.github.com>
2025-02-20 11:25:52 +0300

    Logging middleware function

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 const port = 8080;
-
+// Logging Middleware function
 function requestLogger(requestz, response, next){
     console,log(`Request Method: ${request.method}), URL:${request.url}`);
     next();
