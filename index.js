8b055aab50fe7711da9ea398a521f05a59f0d244
Geno Joshua <133131809+GenoJ83@users.noreply.github.com>
2025-02-20 11:26:47 +0300

    passing control to the next middleware/ route handler

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 8080;
 // Logging Middleware function
 function requestLogger(requestz, response, next){
     console,log(`Request Method: ${request.method}), URL:${request.url}`);
-    next();
+    next();// Passing control to the next middleware/ route handler
 }
 app.use(requestLogger); //we then apply the middleware to all routes
 app.get('/', (req,res)=>{
